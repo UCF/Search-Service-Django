298e6b0fe2e3eac89eeadcbc1cd4a72f8183fdf7
Jim Barnes <jimbarnesdeveloper@gmail.com>
2018-04-11 15:03:47 -0400

    Added some detail to the program list API

--- programs/serializers.py
+++ programs/serializers.py
@@ -43,8 +43,46 @@ class ProgramProfileSerializer(serializers.ModelSerializer):
         fields = '__all__'
         model = ProgramProfile
 
+class ParentProgramSerializer(serializers.ModelSerializer):
+    level = serializers.StringRelatedField(many=False)
+    career = serializers.StringRelatedField(many=False)
+    degree = serializers.StringRelatedField(many=False)
+    colleges = CollegeSerializer(many=True, read_only=True)
+    departments = DepartmentSerializer(many=True, read_only=True)
+
+    class Meta:
+        fields = (
+            'name',
+            'plan_code',
+            'subplan_code',
+            'catalog_url',
+            'colleges',
+            'departments',
+            'level',
+            'career',
+            'degree'
+        )
+        model = Program
 
 class ProgramSerializer(serializers.ModelSerializer):
+    level = serializers.StringRelatedField(many=False)
+    career = serializers.StringRelatedField(many=False)
+    degree = serializers.StringRelatedField(many=False)
+    colleges = CollegeSerializer(many=True, read_only=True)
+    departments = DepartmentSerializer(many=True, read_only=True)
+    parent_program = ParentProgramSerializer(many=False, read_only=True)
+
     class Meta:
-        fields = '__all__'
+        fields = (
+            'name',
+            'plan_code',
+            'subplan_code',
+            'catalog_url',
+            'colleges',
+            'departments',
+            'level',
+            'career',
+            'degree',
+            'parent_program'
+        )
         model = Program
