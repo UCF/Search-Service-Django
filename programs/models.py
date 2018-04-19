5ad4d7474a4bec009aa8207b34a5084f4242baa0
Jim Barnes <james.barnes@ucf.edu>
2018-04-19 09:55:06 -0400

    Added token authentication to project

--- programs/models.py
+++ programs/models.py
@@ -3,6 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
 # Create your models here.
 
 
@@ -191,3 +196,9 @@ class ProgramDescription(models.Model):
         blank=False,
         related_name='descriptions'
     )
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
--- settings.py
+++ settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'programs',
     'rest_framework',
     'django_filters',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -65,10 +66,15 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'programs.authentication.ParamKeyAuthentication',
     )
 }
 
