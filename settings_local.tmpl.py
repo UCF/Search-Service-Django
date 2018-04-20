71135bff26db823b0de10fa3e2a7b1594bc2b80e
Jim Barnes <jimbarnesdeveloper@gmail.com>
2018-04-20 13:56:10 -0400

    Updated with additional local settings for deployment

--- settings_local.tmpl.py
+++ settings_local.tmpl.py
@@ -58,3 +58,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static") # Comment out when using locally
+STATICFILES_DIRS = [
+    # Add static root path when debugging locally
+]
