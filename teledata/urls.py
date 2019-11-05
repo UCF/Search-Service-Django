bbadef28c5c914678fb55ef938b6937e43ff902b
Cadie Brown <cadie.brown@gmail.com>
2019-11-05 14:45:42 -0500

    Added teledata buildings view route

---
+++ teledata/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+
+from teledata.views import *
+
+urlpatterns = [
+    url(r'^buildings/$',
+        BuildingListView.as_view(),
+        name='api.buildings.list'
+    ),
+]
