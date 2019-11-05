632c11cc97371b44b70fc0980235470dc367f8f1
Cadie Brown <cadie.brown@gmail.com>
2019-11-05 14:45:08 -0500

    Add teledata route to main urls.py

--- urls.py
+++ urls.py
@@ -17,13 +17,16 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^api/v1/teledata/',
+        include('teledata.urls')
+    ),
     url(r'^api/v1/',
         include('programs.urls')
-        ),
+    ),
     url(r'^api-auth/',
         include('rest_framework.urls')
         ),
     url(r'^admin/',
         admin.site.urls
-        ),
+    ),
 ]
