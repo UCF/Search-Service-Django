4899da692a0224e4648ff7fd5d41a0a607516549
Cadie Brown <cadie.brown@gmail.com>
2019-11-05 23:48:32 -0500

    Create general routes for Department, Organization, and Staff

--- teledata/urls.py
+++ teledata/urls.py
@@ -7,4 +7,16 @@ urlpatterns = [
         BuildingListView.as_view(),
         name='api.buildings.list'
         ),
+    url(r'^departments/$',
+        DepartmentListView.as_view(),
+        name='api.departments.list'
+        ),
+    url(r'^organizations/$',
+        OrganizationListView.as_view(),
+        name='api.organizations.list'
+        ),
+    url(r'^staff/$',
+        StaffListView.as_view(),
+        name='api.staff.list'
+        ),
 ]
