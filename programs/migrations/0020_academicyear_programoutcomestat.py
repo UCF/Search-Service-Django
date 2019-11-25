452ff4c444b976c16e9fe6f5a9b753687f20f264
Jim Barnes <jimbarnesdeveloper@gmail.com>
2019-11-25 12:34:53 -0500

    Updated migration name

--- programs/migrations/0020_academicyear_programoutcomestat.py
+++ programs/migrations/0020_academicyear_programoutcomestat.py
@@ -9,7 +9,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('programs', '0018_program_active'),
+        ('programs', '0019_program_cip_code'),
     ]
 
     operations = [
