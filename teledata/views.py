3c5cbdd3f0f67e111da0f7dbd2fe491b9ee2e548
Jim Barnes <jimbarnesdeveloper@gmail.com>
2019-12-02 13:14:59 -0500

    Reset commit

--- teledata/views.py
+++ teledata/views.py
@@ -7,10 +7,6 @@ from rest_framework.reverse import reverse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
-from django.urls import resolve
-
-from django.views.generic.base import TemplateView
-from core.views import TitleContextMixin
 
 from teledata.models import *
 from teledata.serializers import *
@@ -61,59 +57,3 @@ class CombinedTeledataSearchView(CombinedTeledataListView):
             return CombinedTeledata.objects.search(self.request.GET.get('search'))
         else:
             return CombinedTeledata.objects.none()
-
-# Documentation Views
-class TeledataMenuMixin(object):
-    pages = [
-        ('docs.teledata.search', 'Teledata Search'),
-        ('docs.teledata.staff.list', 'Staff List'),
-        ('docs.teledata.organizations.list', 'Organization List'),
-        ('docs.teledata.departments.list', 'Department List'),
-        ('docs.teledata.buildings.list', 'Building List')
-    ]
-
-    def get_context_data(self, **kwargs):
-        context = super(TeledataMenuMixin, self).get_context_data(**kwargs)
-        page_list = []
-
-        for page in self.pages:
-            active = False
-            url = reverse(page[0])
-
-            if url == self.request.path:
-                active = True
-
-            page_list.append({
-                'url': url,
-                'text': page[1],
-                'active': active
-            })
-
-        context['menu'] = page_list
-
-        return context
-
-class TeledataSearchDocView(TitleContextMixin, TeledataMenuMixin, TemplateView):
-    template_name = 'teledata/docs/teledata-search.html'
-    title = 'Teledata Documentation'
-    heading = 'Teledata Search'
-
-class TeledataStaffDocView(TitleContextMixin, TeledataMenuMixin, TemplateView):
-    template_name = 'teledata/docs/teledata-staff-list.html'
-    title = 'Staff List Documentation'
-    heading = 'Staff List'
-
-class TeledataOrganizationDocView(TitleContextMixin, TeledataMenuMixin, TemplateView):
-    template_name = 'teledata/docs/teledata-org-list.html'
-    title = 'Organizations Documentation'
-    heading = 'Organization List'
-
-class TeledataDepartmentDocView(TitleContextMixin, TeledataMenuMixin, TemplateView):
-    template_name = 'teledata/docs/teledata-dept-list.html'
-    title = 'Departments Documentation'
-    heading = 'Department List'
-
-class TeledataBuildingDocView(TitleContextMixin, TeledataMenuMixin, TemplateView):
-    template_name = 'teledata/docs/teledata-bldg-list.html'
-    title = 'Buildings List'
-    heading = 'Building List'
