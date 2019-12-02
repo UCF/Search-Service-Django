d13e52cfaae937cd0f63eb5bd27657cce9b72cc7
Jim Barnes <jimbarnesdeveloper@gmail.com>
2019-12-02 14:30:27 -0500

    Take 2 on homepage docs

--- urls.py
+++ urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^api/v1/teledata/',
@@ -28,6 +29,11 @@ urlpatterns = [
         ),
     url(r'^admin/',
         admin.site.urls
+        ),
+    url(
+        r'^favicon.ico$',
+        RedirectView.as_view(url='/static/favicon.ico'),
+        name='favicon'
         ),
     url(r'^',
         include('core.urls')
