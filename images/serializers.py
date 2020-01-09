5ad99aa3a3fad642ced42f42eda644249518b858
Jo Dickson <jogreybill@gmail.com>
2020-01-09 09:21:02 -0500

    Added explicit serializer for image tags

--- images/serializers.py
+++ images/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
+
 from images.models import *
 
 
 class ImageSerializer(serializers.ModelSerializer):
+    tags = serializers.StringRelatedField(many=True)
+
     class Meta:
         fields = '__all__'
         model = Image
