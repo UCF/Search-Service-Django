a08ba05474515c52b522f8bf4dbe0c17138a6e5b
Jo Dickson <jogreybill@gmail.com>
2020-01-10 09:30:40 -0500

    Improved csv mimetype checking and error handling; added helpful info on imported synonyms, skipped tags, and total tags processed

--- images/management/commands/import-tandemvault-tags.py
+++ images/management/commands/import-tandemvault-tags.py
@@ -5,6 +5,7 @@ import csv
 import datetime
 import json
 import logging
+import mimetypes
 import timeit
 
 from progress.bar import Bar
@@ -14,13 +15,15 @@ from unidecode import unidecode
 class Command(BaseCommand):
     help = 'Imports all existing image tags from UCF\'s Tandem Vault instance.'
 
-    progress_bar = Bar('Processing')
-    source       = 'Tandem Vault'
-    tags         = []
-    tag_count    = 0
-    tags_created = 0
-    tags_updated = 0
-    tags_deleted = 0
+    progress_bar      = Bar('Processing')
+    source            = 'Tandem Vault'
+    tags              = []
+    tag_count         = 0
+    tags_created      = 0
+    tags_updated      = 0
+    tags_deleted      = 0
+    tags_skipped      = 0
+    synonyms_assigned = 0
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -69,11 +72,26 @@ class Command(BaseCommand):
     Load a CSV of Tandem Vault tags + synonyms.
     '''
     def load_tandemvault_tags(self):
+        # The csv lib will happily process pretty much any file
+        # you throw at it; do some really rudimentary checking
+        # against the file name before continuing:
+        try:
+            mime = mimetypes.guess_type(self.tandemvault_tags_csv.name)[0]
+        except Exception, e:
+            logging.error(
+                '\n ERROR reading CSV: couldn\'t verify mimetype of file')
+            return
+
+        if mime != 'text/csv':
+            logging.error(
+                '\n ERROR reading CSV: expected file with mimetype "text/csv"; got "%s"' % mime)
+            return
+
         try:
             csv_reader = csv.DictReader(self.tandemvault_tags_csv)
-        except csv.Error:
+        except csv.Error, e:
             logging.error(
-                '\n ERROR reading CSV: CSV does not have valid headers or is malformed.')
+                '\n ERROR reading CSV: %s' % e)
             return
 
         for row in csv_reader:
@@ -90,6 +108,7 @@ class Command(BaseCommand):
             # Empty tags can exist in this data for whatever reason;
             # skip them if present
             if not name:
+                self.tags_skipped += 1
                 continue
 
             synonyms = filter(None, [self.clean_tag_name(synonym) for synonym in json.loads(tandemvault_tag['synonym_names'])])
@@ -109,6 +128,7 @@ class Command(BaseCommand):
             for synonym in synonyms:
                 s, created = ImageTag.objects.get_or_create(name=synonym, source=self.source)
                 tag.synonyms.add(s)
+                self.synonyms_assigned += 1
 
             tag.save()
 
@@ -129,17 +149,22 @@ class Command(BaseCommand):
     '''
     def print_stats(self):
         stats = """
-Finished import of Tandem Vault image tags.
+Finished import of {0} Tandem Vault image tags.
 
-Created: {0}
-Updated: {1}
-Deleted: {2}
+Created: {1}
+Updated: {2}
+Deleted: {3}
+Skipped: {4}
+Synonyms assigned: {5}
 
-Script executed in {3}
+Script executed in {6}
         """.format(
+            self.tag_count,
             self.tags_created,
             self.tags_updated,
             self.tags_deleted,
+            self.tags_skipped,
+            self.synonyms_assigned,
             self.exec_time
         )
 
