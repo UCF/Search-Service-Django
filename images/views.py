2fe11a7294491f1907294236b78771ffe32b12d3
Jo Dickson <jogreybill@gmail.com>
2020-01-13 14:02:33 -0500

    Added ordering by photo_taken value

--- images/views.py
+++ images/views.py
@@ -24,5 +24,5 @@ class ImageDetailView(generics.RetrieveAPIView):
 class ImageSearchView(ImageListView):
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['filename', 'tags__name', 'tags__synonyms__name', 'caption']
-    ordering_fields = ['source_created', 'source_modified']
-    ordering = ['-source_modified']
+    ordering_fields = ['source_created', 'source_modified', 'photo_taken']
+    ordering = ['-photo_taken']
