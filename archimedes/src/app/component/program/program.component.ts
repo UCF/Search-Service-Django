4975d1c2aa0d78850b47b51f326ef8052cbef12b
RJ Bruneel <rj.bruneel@ucf.edu>
2020-01-15 13:18:26 -0500

    updated url if statement

--- archimedes/src/app/component/program/program.component.ts
+++ archimedes/src/app/component/program/program.component.ts
@@ -15,7 +15,7 @@ export class ProgramComponent implements OnInit {
   getUrl(program) {
     if (program.catalog_url) {
       return program.catalog_url;
-    } else if (program.profiles[0].url) {
+    } else if (program.profiles && program.profiles.length && program.profiles[0].url) {
       return program.profiles[0].url;
     } else {
       return null;
