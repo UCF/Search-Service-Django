214001d36d6e8c0def969553ebdcae77704e4fbd
RJ Bruneel <rj.bruneel@ucf.edu>
2020-01-23 15:47:22 -0500

    refactored to simplify code

--- archimedes/src/app/component/show-more/show-more.component.ts
+++ archimedes/src/app/component/show-more/show-more.component.ts
@@ -14,71 +14,45 @@ export class ShowMoreComponent implements OnInit {
   @Input() searchType: string;
   @Input() limit: number;
 
-  @Output() programLoading: EventEmitter<boolean> = new EventEmitter<boolean>();
-  @Output() programError: EventEmitter<boolean> = new EventEmitter<boolean>();
-  @Output() programResults: EventEmitter<any> = new EventEmitter<any>();
-
-  @Output() newsLoading: EventEmitter<boolean> = new EventEmitter<boolean>();
-  @Output() newsError: EventEmitter<boolean> = new EventEmitter<boolean>();
-  @Output() newsResults: EventEmitter<any> = new EventEmitter<any>();
-
-  @Output() imageLoading: EventEmitter<boolean> = new EventEmitter<boolean>();
-  @Output() imageError: EventEmitter<boolean> = new EventEmitter<boolean>();
-  @Output() imageResults: EventEmitter<any> = new EventEmitter<any>();
+  @Output() loading: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() error: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() results: EventEmitter<any> = new EventEmitter<any>();
 
   constructor(
     private httpService: HttpService
-  ) {}
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
-  showMore(offset) {
+  showMoreResults(offset) {
     this.offset = this.offset + offset;
-    if(this.searchType === 'programs') {
-      this.programLoading.emit(true);
-      this.showMoreResults(this.searchType, this.programLoading, this.programError, this.programResults);
-    }
-    if(this.searchType === 'news') {
-      this.newsLoading.emit(true);
-      this.showMoreResults(this.searchType, this.newsLoading, this.newsError, this.newsResults);
-    }
-    if(this.searchType === 'images') {
-      this.imageLoading.emit(true);
-      this.showMoreResults(this.searchType, this.imageLoading, this.imageError, this.imageResults);
-    }
-  }
-
-  showMoreResults(
-    searchType: string,
-    loading: EventEmitter<boolean>,
-    error: EventEmitter<boolean>,
-    results: EventEmitter<any>) {
+    this.loading.emit(true);
 
     // convert the `click` event into an observable stream
-    this.httpService.search(searchType, this.query, this.offset.toString())
+    this.httpService.search(this.searchType, this.query, this.offset.toString())
       .subscribe(
         (response: any) => { // on success
-          loading.emit(false);
-          error.emit(false);
+          this.loading.emit(false);
+          this.error.emit(false);
           // news
           if (response.headers.get('X-WP-Total')) {
-            results.emit({
+            this.results.emit({
               "results": response.body,
               "count": response.headers.get('X-WP-Total')
             });
             // program or images
           } else {
-            results.emit(response.body)
+            this.results.emit(response.body)
           }
         },
         (error: any) => { // on error
           console.error(error);
-          loading.emit(false);
-          error.emit(true);
+          this.loading.emit(false);
+          this.error.emit(true);
         },
         () => { // on completion
-          loading.emit(false);
-          error.emit(false);
+          this.loading.emit(false);
+          this.error.emit(false);
         }
       );
   }
