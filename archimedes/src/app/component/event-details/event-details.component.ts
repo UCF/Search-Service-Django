ba82440ec3c96594aa44a59d5e943f4b8f2320c4
Jo Dickson <jogreybill@gmail.com>
2020-02-28 14:46:23 -0500

    Added event details

--- archimedes/src/app/component/event-details/event-details.component.ts
+++ archimedes/src/app/component/event-details/event-details.component.ts
@@ -1,23 +1,23 @@
-import { EventDetailService } from './../../service/event-detail.service';
+import { EventDetailsService } from '../../service/event-details.service';
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-event-details',
+  selector: '[app-event-details]',
   templateUrl: './event-details.component.html',
   styleUrls: ['./event-details.component.scss']
 })
 export class EventDetailsComponent implements OnInit {
-  @Input() result: any;
+  @Input() eventResult: any;
 
   eventDetails: any;
 
   constructor(
-    private eventDetailService: EventDetailService
+    private eventDetailService: EventDetailsService
   ) {  }
 
   ngOnInit() {
 
-    this.eventDetailService.getEventDetails( event )
+    this.eventDetailService.getEventDetails(this.eventResult)
       .subscribe(
         (response: any) => { // on success
           this.eventDetails = response;
