76dd43905da374d7d7ebe38cf0405052295ace2d
Jo Dickson <jogreybill@gmail.com>
2020-05-05 15:53:29 -0400

    Fixed borked deadline month/day choices

--- programs/migrations/0037_auto_20200427_1949.py
+++ programs/migrations/0037_auto_20200427_1949.py
@@ -32,8 +32,8 @@ class Migration(migrations.Migration):
             name='ApplicationDeadline',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('month', models.IntegerField(choices=[(1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10), (11, 11), (12, 12), (13, 13), (14, 14), (15, 15), (16, 16), (17, 17), (18, 18), (19, 19), (20, 20), (21, 21), (22, 22), (23, 23), (24, 24), (25, 25), (26, 26), (27, 27), (28, 28), (29, 29), (30, 30), (31, 31)])),
-                ('day', models.IntegerField(choices=[(1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10), (11, 11)])),
+                ('month', models.IntegerField(choices=[(1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10), (11, 11), (12, 12)])),
+                ('day', models.IntegerField(choices=[(1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10), (11, 11), (12, 12), (13, 13), (14, 14), (15, 15), (16, 16), (17, 17), (18, 18), (19, 19), (20, 20), (21, 21), (22, 22), (23, 23), (24, 24), (25, 25), (26, 26), (27, 27), (28, 28), (29, 29), (30, 30), (31, 31)])),
                 ('admission_term', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='application_deadlines', to='programs.AdmissionTerm')),
                 ('career', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='application_deadlines', to='programs.Career')),
                 ('deadline_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='application_deadlines', to='programs.AdmissionDeadlineType')),
