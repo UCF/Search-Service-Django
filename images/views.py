e5ee4aadecac7f50c3c39a78e9bcb5957ba787d1
Jo Dickson <jogreybill@gmail.com>
2020-10-01 10:52:38 -0400

    Remove self.ordering on ImageSearchView

--- images/views.py
+++ images/views.py
@@ -26,14 +26,13 @@ class ImageSearchView(generics.ListAPIView):
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['tags__name', 'tags__synonyms__name', 'caption', 'location']
     ordering_fields = ['score', 'source_created', 'source_modified', 'photo_taken']
-    ordering = ['-score', '-photo_taken']
 
     def get_ordering(self):
         if self.request.GET.get('search') is None:
             # There is no score because the query isn't run;
             # remove ordering by score
             return ['-photo_taken']
-        return self.ordering
+        return ['-score', '-photo_taken']
 
     def get_queryset(self):
         if self.request.GET.get('search') is not None:
