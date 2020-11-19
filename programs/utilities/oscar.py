57fcbb92535e1a3c55c8a6d2c298a6a241924fb6
Jim Barnes <jimbarnesdeveloper@gmail.com>
2020-11-19 09:43:48 -0500

    Added in the content_node class and the shell for oscar.

---
+++ programs/utilities/oscar.py
@@ -0,0 +1,31 @@
+import boto3
+import settings
+
+
+
+class Oscar:
+
+    #region Shared Class Variables
+
+    SKIP = [
+
+    ]
+
+    #endregion
+
+    #region Initializer
+
+    def __init__(self):
+        pass
+
+    #endregion
+
+    #region Private Functions
+
+    #endregion
+
+    #region Public functions
+
+    def
+
+    #endregion
