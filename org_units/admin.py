260504a8e2d4f7042560df6c9c8d4c391752ac9f
Jo Dickson <jogreybill@gmail.com>
2021-06-09 16:19:13 -0400

    Added admin interfaces for depts, orgs

--- org_units/admin.py
+++ org_units/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from org_units.models import *
+
 # Register your models here.
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    pass
