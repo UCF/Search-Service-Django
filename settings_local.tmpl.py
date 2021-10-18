eef9ebc47782216ddd51fcda9e43f0d9392aaaa6
Jim Barnes <jimbarnesdeveloper@gmail.com>
2021-10-18 14:21:10 -0400

    Removed special logging as it was not working

--- settings_local.tmpl.py
+++ settings_local.tmpl.py
@@ -95,11 +95,6 @@ LOGGING = {
             'level': 'WARNING',
             'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler'
         },
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': BASE_DIR + '%(app)_%(class).log'
-        },
         'django.server': DEFAULT_LOGGING['handlers']['django.server'],
         'programs.management.commands': DEFAULT_LOGGING['handlers']['django.server'],
         'teledata.management.commands': DEFAULT_LOGGING['handlers']['django.server'],
@@ -112,11 +107,6 @@ LOGGING = {
         'django.server': DEFAULT_LOGGING['loggers']['django.server'],
         'programs.management.commands': DEFAULT_LOGGING['loggers']['django.server'],
         'teledata.management.commands': DEFAULT_LOGGING['loggers']['django.server'],
-        'research.management.commands': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True
-        }
     }
 }
 
