16e1aaa0fbda159383955327db1df7bc952d4638
Jo Dickson <jogreybill@gmail.com>
2022-04-29 09:37:51 -0400

    Tidying up

--- locations/views.py
+++ locations/views.py
@@ -8,64 +8,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 
-from teledata.models import *
-from teledata.serializers import *
-from teledata.filters import *
-from teledata.pagination import BackwardsCompatiblePagination
+from locations.models import *
+from locations.serializers import *
+# from locations.filters import *
 
 from rest_framework.filters import OrderingFilter
 
 
 # Create your views here.
 
-# class BuildingListView(generics.ListAPIView):
-#     queryset = Building.objects.all()
-#     serializer_class = BuildingSerializer
-#     filter_backends = [DjangoFilterBackend, OrderingFilter]
-#     ordering_fields = ['id', 'name']
-
-
-# class DepartmentListView(generics.ListAPIView):
-#     filter_class = DepartmentFilter
-#     queryset = Department.objects.all()
-#     serializer_class = DepartmentSerializer
-#     filter_backends = [DjangoFilterBackend, OrderingFilter]
-#     ordering_fields = ['id', 'name']
-
-
-# class DepartmentDetailView(generics.RetrieveAPIView):
-#     queryset = Department.objects.all()
-#     lookup_field = 'id'
-#     serializer_class = DepartmentSerializer
-
-
-# class OrganizationListView(generics.ListAPIView):
-#     queryset = Organization.objects.all()
-#     serializer_class = OrganizationSerializer
-#     filter_backends = [DjangoFilterBackend, OrderingFilter]
-#     ordering_fields = ['id', 'name']
-
-
-# class StaffListView(generics.ListAPIView):
-#     queryset = Staff.objects.all()
-#     serializer_class = StaffSerializer
-
-
-# class CombinedTeledataListView(generics.ListAPIView):
-#     queryset = CombinedTeledata.objects.all()
-#     serializer_class = CombinedTeledataSerializer
-
-
-# class CombinedTeledataSearchView(CombinedTeledataListView):
-#     filter_class = CombinedTeledataFilter
-#     filter_backends = [DjangoFilterBackend, OrderingFilter]
-#     pagination_class = BackwardsCompatiblePagination
-#     ordering_fields = ['id', 'score', 'sort_name']
-
-#     def get_queryset(self):
-#         search_query = self.request.GET.get('search')
-#         if search_query:
-#             return CombinedTeledata.objects.search(search_query).order_by('-score')
-#         else:
-#             # There is no score because the query isn't run. Order by id
-#             return CombinedTeledata.objects.none().order_by('id')
+# TODO
