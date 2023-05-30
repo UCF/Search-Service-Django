148a206a791caa21a4bbafd6b78764cb5a04dbf2
Jim Barnes <jimbarnesdeveloper@gmail.com>
2023-05-30 14:03:47 -0400

    Added logic to write attributes to user object

--- core/saml_hooks.py
+++ core/saml_hooks.py
@@ -1,14 +1,13 @@
 from django.contrib.auth.models import User
 
 def on_saml_user_create(user: User, saml_data: dict):
-    user.first_name = saml_data['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname']
-    user.last_name = saml_data['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname']
-    user.email = saml_data['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress']
+    user.first_name = saml_data['givenName']
+    user.last_name = saml_data['surname']
+    user.email = saml_data['emailAddress']
     user.save()
 
 def on_saml_before_login(user: User, saml_data: dict):
-    user.first_name = saml_data['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname']
-    user.last_name = saml_data['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname']
-    user.email = saml_data['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress']
-
+    user.first_name = saml_data['givenName']
+    user.last_name = saml_data['surname']
+    user.email = saml_data['emailAddress']
     user.save()
