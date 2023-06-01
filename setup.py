52ada50145f03f8a62e32b9df2316d26615ac7c7
Jim Barnes <jimbarnesdeveloper@gmail.com>
2023-06-01 10:57:30 -0400

    Bumped version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='UCF-Search-Service-Django',
-    version='2.6.6',
+    version='3.0.0',
     description='Django API for various UCF data',
     long_description=long_description,
     long_description_content_type='text/markdown',
