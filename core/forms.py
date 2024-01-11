936e2881d6c60071733783d24b2bd90ffaab155e
Jim Barnes <jimbarnesdeveloper@gmail.com>
2024-01-11 10:27:27 -0500

    Updated styles

--- core/forms.py
+++ core/forms.py
@@ -2,9 +2,6 @@ from django import forms
 
 from core.widgets import Wysiwyg
 
-class JobsField(forms.MultipleHiddenInput):
-    pass
-
 class CommunicatorProgramForm(forms.Form):
     custom_description = forms.CharField(widget=Wysiwyg(), required=False)
     jobs = forms.CharField()
