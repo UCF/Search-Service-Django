032a430cfe2645f8e94d57ae84040b69db253821
Jim Barnes <jimbarnesdeveloper@gmail.com>
2024-03-04 15:05:56 -0500

    Updated the form to save the jobs_source field

--- core/forms.py
+++ core/forms.py
@@ -5,4 +5,4 @@ from core.widgets import Wysiwyg
 class CommunicatorProgramForm(forms.Form):
     custom_description = forms.CharField(widget=Wysiwyg(), required=False)
     jobs = forms.CharField()
-
+    jobs_source = forms.CharField(widget=forms.Textarea(attrs={ 'rows': 5 }), required=True)
