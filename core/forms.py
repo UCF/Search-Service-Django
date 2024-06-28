7b68a94dcee88a4c2dad3406d7259de26e99142e
Ramin Farhadi <ra680091@CMN9J7HFG4MN.attlocal.net>
2024-06-28 10:43:04 -0400

    from field was removed from this pr.

--- core/forms.py
+++ core/forms.py
@@ -6,4 +6,3 @@ class CommunicatorProgramForm(forms.Form):
     custom_description = forms.CharField(widget=Wysiwyg(), required=False)
     jobs = forms.CharField()
     jobs_source = forms.CharField(widget=forms.Textarea(attrs={ 'rows': 5 }), required=True)
-    highlight_description1 = forms.CharField(widget=forms.Textarea(attrs= { 'rows' : 4 }))
