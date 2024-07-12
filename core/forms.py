7de032c5c7dc511f628271448a8613e84b434b8f
Ramin Farhadi <ra680091@CMN9J7HFG4MN.attlocal.net>
2024-07-12 09:42:33 -0400

    added the highlight section

--- core/forms.py
+++ core/forms.py
@@ -6,3 +6,4 @@ class CommunicatorProgramForm(forms.Form):
     custom_description = forms.CharField(widget=Wysiwyg(), required=False)
     jobs = forms.CharField()
     jobs_source = forms.CharField(widget=forms.Textarea(attrs={ 'rows': 5 }), required=True)
+    highlights = forms.CharField(widget=forms.HiddenInput)
