ab218778092f6c5328bac3f9689d0be2c5ce16b0
Ramin Farhadi <ra680091@CMN9J7HFG4MN.attlocal.net>
2024-07-22 14:51:01 -0400

    default value added to highlights field

--- programs/models.py
+++ programs/models.py
@@ -527,7 +527,7 @@ class Program(models.Model):
     start_term = models.ForeignKey(AcademicTerm, null=True, blank=True, related_name='start_term_programs', on_delete=models.SET_NULL)
     jobs = models.ManyToManyField(JobPosition, related_name='programs')
     history = AuditlogHistoryField()
-    highlights = models.JSONField(null=True, blank=True)
+    highlights = models.JSONField(default=list, null=False, blank=False)
 
 
     class Meta:
