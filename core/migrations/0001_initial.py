30648ab72f85184e50e4c3e1b4b7a6c64d3925b4
Jim Barnes <jimbarnesdeveloper@gmail.com>
2025-01-15 16:50:37 -0500

    Ensure programs migrations run first

--- core/migrations/0001_initial.py
+++ core/migrations/0001_initial.py
@@ -22,6 +22,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('programs', '0062_auto_20240724_1513')
     ]
 
     operations = [
