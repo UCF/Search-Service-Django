c3a09851bcaff8487180c56f2d6414854ad333c6
Ramin Farhadi <ra680091@CMN9J7HFG4MN.local>
2025-01-22 14:41:41 -0500

    Close button will be displayed dynamically. When icon and description section are empty, it will be hidden.

--- src/js/highlights-iconpicker.js
+++ src/js/highlights-iconpicker.js
@@ -1,45 +1,45 @@
 let myData;
 let filteredIcons;
-let tempIcon = "";
+let tempIcon = '';
 let tempOrder = -1;
 
-const modalIconListContainer = document.querySelector(".icon-list-container");
-const highlightsWrapper = document.querySelector("#highlights-wrapper");
+const modalIconListContainer = document.querySelector('.icon-list-container');
+const highlightsWrapper = document.querySelector('#highlights-wrapper');
 const highlightsField = document.querySelector('input[name="highlights"]');
 
 let highlightsObj = [];
 
 const initialHighlightValue = highlightsField.value;
 
-if (initialHighlightValue !== "") {
+if (initialHighlightValue !== '') {
   highlightsObj = JSON.parse(initialHighlightValue);
 } else {
   highlightsObj = [];
 }
 
 const iconList = () => {
-  modalIconListContainer.innerHTML = "";
+  modalIconListContainer.innerHTML = '';
   const iconArray = filteredIcons ? filteredIcons : myData;
 
   for (let j = 0; j < 30; j++) {
     const iconName = iconArray[j];
-    const iconElement = document.createElement("i");
-    const colDiv = document.createElement("div");
+    const iconElement = document.createElement('i');
+    const colDiv = document.createElement('div');
 
-    iconElement.addEventListener("click", (e) => modalIconClassPicker(e));
+    iconElement.addEventListener('click', (e) => modalIconClassPicker(e));
 
-    colDiv.classList.add("col-2");
-    colDiv.setAttribute("role", "button");
+    colDiv.classList.add('col-2');
+    colDiv.setAttribute('role', 'button');
 
-    iconElement.classList.add(iconName, "fa", "m-2");
-    iconElement.style = "font-size: 3rem";
+    iconElement.classList.add(iconName, 'fa', 'm-2');
+    iconElement.style = 'font-size: 3rem';
 
     colDiv.appendChild(iconElement);
     modalIconListContainer.appendChild(colDiv);
   }
 };
 
-fetch("/static/js/fontawesome-v6.4.2.json")
+fetch('/static/js/fontawesome-v6.4.2.json')
   .then((res) => res.json())
   .then((data) => {
     myData = data.solid;
@@ -54,8 +54,8 @@ const addStory = (event) => {
       : -1;
   highlightsObj.push({
     data_order: lastDataOrder + 1,
-    icon_class: "",
-    description: "",
+    icon_class: '',
+    description: ''
   });
   updateHighlightsWrapper();
 };
@@ -64,14 +64,23 @@ const updateHighlightsWrapper = () => {
   if (highlightsObj.length === 0) {
     highlightsObj.push({
       data_order: 0,
-      icon_class: "",
-      description: "",
+      icon_class: '',
+      description: ''
     });
   }
 
   const highlightsMarkUp = highlightsObj.map((item) => {
     const dynamicClass =
-      item.icon_class || "text-muted fa-cloud-arrow-up fa m-2";
+      item.icon_class || 'text-muted fa-cloud-arrow-up fa m-2';
+
+    // if highlights section is empty, close button will be hidden./
+    const hideCloseButtonClass =
+      item.icon_class === '' &&
+      item.description.trim() === '' &&
+      highlightsObj.length === 1
+        ? 'd-none'
+        : '';
+
     return `<div data-order="${item.data_order}" class="row mb-4">
       <div class="col-3">
         <label for="icon-field-picker" class="h6">Icon</label>
@@ -86,11 +95,11 @@ const updateHighlightsWrapper = () => {
           <textarea id="highlights-des-textarea" class="form-control" rows="7" maxlength="150" onkeyup="descriptionHandler(${item.data_order}, event)">${item.description}</textarea>
         </div>
       </div>
-      <div class="col-1 position-relative"><button type="button" class="btn-close p-3 position-absolute top-0 start-0" aria-label="Close" onclick="removeHighlight(${item.data_order})"></button>
+      <div class="col-1 position-relative"><button type="button" class="btn-close p-3 position-absolute top-0 start-0 ${hideCloseButtonClass}" aria-label="Close" onclick="removeHighlight(${item.data_order})"></button>
       </div>
     </div>`;
   });
-  highlightsWrapper.innerHTML = highlightsMarkUp.join("");
+  highlightsWrapper.innerHTML = highlightsMarkUp.join('');
   highlightsField.value = JSON.stringify(highlightsObj);
 };
 updateHighlightsWrapper();
@@ -122,21 +131,21 @@ const modalIconSelectBtn = () => {
 
 const modalIconClassPicker = (event) => {
   const iconClassArray = event.target.classList;
-  const faClass = iconClassArray[0].replace("fa-", "");
+  const faClass = iconClassArray[0].replace('fa-', '');
 
   // Check if the clicked icon is already selected
   if (tempIcon === faClass) {
     // Deselect the icon
-    tempIcon = ""; // Reset the tempIcon
-    event.target.classList.remove("bg-primary", "p-1"); // Remove highlight classes
+    tempIcon = ''; // Reset the tempIcon
+    event.target.classList.remove('bg-primary', 'p-1'); // Remove highlight classes
   } else {
     // Select the icon
     modalIconListContainer.childNodes.forEach((element) => {
-      element.firstChild.classList.remove("bg-primary", "p-1");
+      element.firstChild.classList.remove('bg-primary', 'p-1');
     });
 
     tempIcon = faClass; // Update the selected icon
-    event.target.classList.add("bg-primary", "p-1"); // Highlight the new selection
+    event.target.classList.add('bg-primary', 'p-1'); // Highlight the new selection
   }
 };
 
@@ -162,7 +171,7 @@ const removeHighlight = (order) => {
     highlightsObj.forEach((item, index) => {
       item.data_order = index;
     });
-
+    console.log(highlightsObj);
     updateHighlightsWrapper(); // Update the UI after removing the item
   }
 };
