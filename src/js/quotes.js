73547d41e262ed394ea8b52b87e609e59ec67a40
Ramin Farhadi <ra680091@CMN9J7HFG4MN.local>
2025-03-06 16:59:58 -0500

    added the Patch request for Editing the image + Updating the UI after uploading the image.

--- src/js/quotes.js
+++ src/js/quotes.js
@@ -1,7 +1,8 @@
 const baseUrl = window.location.origin;
-const activeQuotes = document.querySelectorAll('.active-quotes');
-
 const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+const programId = document.querySelector('[name=quote-section]').getAttribute('data-program-id');
+
+const activeQuotes = document.querySelectorAll('.active-quotes');
 
 const modal = document.getElementById('activeQuoteModal');
 const modalTitle = modal.querySelector('.modal-title');
@@ -11,8 +12,10 @@ const editModalSaveBtn = modal.querySelector('#editModalSaveBtn');
 
 activeQuotes.forEach((quote) => {
   const editButton = quote.querySelector('.active-quote-edit');
+  const detachButton = quote.querySelector('.active-quote-detach');
   const quoteId = quote.getAttribute('data-quote-id');
 
+  // Edit button click event
   editButton.addEventListener('click', () => {
     const quoteSource = quote.querySelector('.active-quotes-quoteSource').innerText;
     const quoteText = quote.querySelector('.active-quotes-quoteText').innerHTML;
@@ -21,6 +24,7 @@ activeQuotes.forEach((quote) => {
     document.getElementById('quoteText').value = quoteText;
     document.getElementById('quoteSource').value = quoteSource;
     document.getElementById('quoteTitle').value = quoteTitle;
+
     $(modal).modal('show');
 
     editModalSaveBtn.onclick = async () => {
@@ -35,35 +39,71 @@ activeQuotes.forEach((quote) => {
       quote.querySelector('.active-quotes-quoteSource').innerHTML = `<strong>${updatedQuoteSource}<strong>`;
       quote.querySelector('.active-quotes-quoteTitle').innerHTML = updatedQuoteTitle;
 
+      if (!updatedQuoteImage) {
       // Prepare the data for the API request
-      const quotePayload = {
-        quote_text: updatedQuoteText,
-        source: updatedQuoteSource,
-        titles: updatedQuoteTitle,
-        tags:['test']
-      };
+        const quotePayload = {
+          quote_text: updatedQuoteText,
+          source: updatedQuoteSource,
+          titles: updatedQuoteTitle,
+          tags:['test']
+        };
+        // Send the API request to update the quote
+        try {
+          const response = await fetch(`${baseUrl}/api/v1/marketing/quotes/${quoteId}/`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+              'X-CSRFToken': csrftoken
+            },
+            body: JSON.stringify(quotePayload)
+          });
 
-      // Send the API request to update the quote
-      try {
-        const response = await fetch(`${baseUrl}/api/v1/marketing/quotes/${quoteId}/`, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrftoken
-          },
-          body: JSON.stringify(quotePayload)
-        });
+          if (response.ok) {
+            const data = await response.json();
+            console.log('Quote updated successfully:', data);
+            $(modal).modal('hide');
+          } else {
+            console.error('Failed to update quote');
+          }
 
-        if (response.ok) {
-          const data = await response.json();
-          console.log('Quote updated successfully:', data);
-          $(modal).modal('hide');
-        } else {
-          console.error('Failed to update quote');
+        } catch (error) {
+          console.error('Error updating quote:', error);
+        }
+      } else if (updatedQuoteImage) {
+        // Prepare the data for the API request
+        const formData = new FormData();
+        formData.append('quote_text', updatedQuoteText);
+        formData.append('source', updatedQuoteSource);
+        formData.append('titles', updatedQuoteTitle);
+        formData.append('tags', 'test');
+        formData.append('image', updatedQuoteImage);
+
+
+        try {
+          const response = await fetch(`${baseUrl}/api/v1/marketing/quotes/${quoteId}/`, {
+            method: 'PATCH',
+            headers: {
+              'X-CSRFToken': csrftoken
+            },
+            body: formData
+          });
+
+          if (response.ok) {
+            const data = await response.json();
+            quote.querySelector('.active-quotes-quoteImage').src = `${data.image}`;
+
+            // Clear the file input field
+            document.getElementById('quoteImage').value = '';
+
+            $(modal).modal('hide');
+          } else {
+            console.error('Failed to update quote');
+          }
+
+        } catch (error) {
+          console.error('Error updating quote:', error);
         }
 
-      } catch (error) {
-        console.error('Error updating quote:', error);
       }
     };
   });
