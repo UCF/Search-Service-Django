5bac59bab6537b5b58de4bb20f760dca194061c1
Ramin Farhadi <ra680091@CMN9J7HFG4MN.local>
2025-06-03 14:21:14 -0400

    imported the status object.

--- marketing/views.py
+++ marketing/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
+from rest_framework import status
 
 from marketing.models import *
 from marketing.serializers import *
