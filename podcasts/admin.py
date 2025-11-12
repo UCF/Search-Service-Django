3658a692371939ae869b27eea2e83b46b0e292fb
Jim Barnes <jimbarnesdeveloper@gmail.com>
2025-11-12 10:08:04 -0500

    Added admin screens

--- podcasts/admin.py
+++ podcasts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from podcasts.models import (
+    PodcastShow,
+    PodcastEpisode
+)
+
 # Register your models here.
+@admin.register(PodcastShow)
+class PodcastShowAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PodcastEpisode)
+class PodcastEpisodeAdmin(admin.ModelAdmin):
+    pass
