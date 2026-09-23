6eb873421dfdd22cddf407e859cd9078f39d7b6e
Darren <darren.johnson@ucf.edu>
2026-09-23 14:42:49 -0400

    Removed archimedes and updated referances

--- settings_local.tmpl.py
+++ settings_local.tmpl.py
@@ -9,9 +9,6 @@ SECRET_KEY = 'SUPERSECRETKEYHEREPLEASEANDTHANKYOU'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# Set to true for local development with the Angular app
-LOCAL = False
-
 ALLOWED_HOSTS = []
 
 # Database
@@ -194,15 +191,6 @@ TANDEMVAULT_ADMIN_API_KEY = ''
 
 # An API key to use with UCF's Tandem Vault that has UCF Communicator-level user access.
 TANDEMVAULT_COMMUNICATOR_API_KEY = ''
-
-# The URL to the News API
-UCF_NEWS_API = 'https://www.ucf.edu'
-
-# The URL to the Events System API
-UCF_EVENTS_API = 'https://events.ucf.edu'
-
-# The URL to the Search Service API
-UCF_SEARCH_SERVICE_API = 'https://search.cm.ucf.edu'
 
 # An AWS Access Key ID with sufficient permissions to access Rekognition.
 AWS_ACCESS_KEY = ''
